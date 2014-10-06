19fbb097a254f43bd53fd1e48b2b4df1fe8b9e1c
Chunqi Shi <chqshi@gmail.com>
2014-10-06 02:22:24 +0100

    added tokenzier

---
+++ src/tokenizer/mr_tokenizer.py
@@ -0,0 +1,31 @@
+###
+#  $ python mr_tokenizer.py doc_list.txt
+###
+
+"""
+"""
+from mrjob.job import MRJob
+from nltk.tokenize import WhitespaceTokenizer
+import re, os
+
+## sentence detection
+SENT_RE = re.compile(r'([A-Z][^\.!?]*[\.!?])', re.M)
+
+class MRTokenizer(MRJob):
+    def mapper(self, _, docname):
+        doc = ''.join(open(docname).readlines()) 
+        ## read document into sentences.			
+        sentences = SENT_RE.findall(doc)
+        for sentence in sentences:
+            yield (sentence, docname)
+
+    def combiner(self, sentence, docnames):
+        ## merge sentence
+        yield (sentence, ', '.join(docnames))
+
+    def reducer(self, sentence, docnames):
+        tokens = WhitespaceTokenizer().tokenize(sentence)
+        yield (', '.join(docnames)+': '+sentence+' --> ', tokens)
+
+if __name__ == '__main__':
+     MRTokenizer.run()
