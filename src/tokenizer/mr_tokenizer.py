095aff3d0602662ecdbc67fbb0d66271c8959475
Chunqi Shi <chqshi@gmail.com>
2014-10-06 17:14:30 +0100

    added global parameters

--- src/tokenizer/mr_tokenizer.py
+++ src/tokenizer/mr_tokenizer.py
@@ -10,10 +10,11 @@ import re, os
 
 ## sentence detection
 SENT_RE = re.compile(r'([A-Z][^\.!?]*[\.!?])', re.M)
+DOC_DIR = os.path.abspath(os.path.dirname(__file__));
 
 class MRTokenizer(MRJob):
     def mapper(self, _, docname):
-        doc = ''.join(open(docname).readlines()) 
+        doc = ''.join(open(os.path.join(DOC_DIR, docname)).readlines()) 
         ## read document into sentences.			
         sentences = SENT_RE.findall(doc)
         for sentence in sentences:
